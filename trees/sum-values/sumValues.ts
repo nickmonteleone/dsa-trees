798673b18201528e4f509729d1ea5d58a107392b
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-02-28 12:13:11 -0800

    Tests pass for sumValues

--- trees/sum-values/sumValues.ts
+++ trees/sum-values/sumValues.ts
@@ -3,7 +3,14 @@ import { TreeNodeNum } from "../common/tree";
 /** sumValues(): add up all values of node and its descendants.
  * Returns sum as an integer. */
 function sumValues(node: TreeNodeNum): number {
-  return 42;
+  if (node === null) return 0;
+
+  let sum = node.val;
+
+  for (const c of node.children) {
+    sum += sumValues(c);
+  }
+  return sum;
 }
 
 export { sumValues };
