3da6c318f8b1ec2f4ba376f4a01f80db44bea529
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-02-28 17:25:26 -0800

    Starter code for binary search trees

---
+++ bsts/insert-recur/insertRecur.ts
@@ -0,0 +1,10 @@
+import { BSTNum } from "../common/bst";
+
+/** insertRecursively(val): Insert a new node into the BST with value val.
+ * Uses recursion. */
+
+function insertRecursively(bst: BSTNum, val: number): void {
+}
+
+export { insertRecursively };
+
