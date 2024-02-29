ea9ab29c2c8822d4d1ace450ed9dae860f3259aa
Max Shpungin <maxsconsulting@gmail.com>
2024-02-29 14:23:52 -0800

    completed search.ts exercises

--- graphs/search/search.ts
+++ graphs/search/search.ts
@@ -1,11 +1,13 @@
 import { GNodeStr } from "../graph/graph";
+import { Stack } from "../common/stack";
+import { Queue } from "../common/queue";
 
 /** Return array of nodes, in DFS order (recursive version)  */
 
 function rDfs(
-    start: GNodeStr,
-    result: string[] = [],
-    visited = new Set([start])): string[] {
+  start: GNodeStr,
+  result: string[] = [],
+  visited = new Set([start])): string[] {
 
   visited.add(start);
   result.push(start.value);
@@ -21,13 +23,47 @@ function rDfs(
 /** Return array of nodes, in DFS order (iterative version)  */
 
 function iDfs(start: GNodeStr): string[] {
-  return ["todo"];
+  const result: string[] = [];
+  const toVisit = new Stack([start]);
+  const visited = new Set([start]);
+
+  while (!toVisit.isEmpty()) {
+    let current = toVisit.pop();
+
+    result.push(current.value);
+
+    for (let child of current.adjacent) {
+      if (!visited.has(child)) {
+        toVisit.push(child);
+        visited.add(child);
+      }
+    }
+  }
+
+  return result;
 }
 
 /** Return array of nodes, in BFS order (iterative version)  */
 
 function bfs(start: GNodeStr): string[] {
-  return ["todo"];
+  const toVisit = new Queue([start])
+  const visited = new Set([start]);
+  const result: string[] = [];
+
+  while(!toVisit.isEmpty()){
+    let current = toVisit.dequeue();
+
+    result.push(current.value);
+
+    for (let child of current.adjacent){
+      if (!visited.has(child)){
+        toVisit.enqueue(child);
+        visited.add(child);
+      }
+    }
+  }
+
+  return result;
 }
 
 
