eb4a3e1bff051936b45df58c08cb1af6542545c3
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-02-28 16:27:28 -0800

    tree pre post order passes

--- trees/pre-post-order/prePostOrder.ts
+++ trees/pre-post-order/prePostOrder.ts
@@ -18,15 +18,16 @@ function preOrder(node: TreeNodeNum | null): number[] {
 /** dfsPostOrder(): Traverse from the invoking node using post-order DFS.
  * Returns an array of visited nodes. */
 
-function postOrder(node: BNodeNum | null): number[] {
+function postOrder(node: TreeNodeNum | null): number[] {
   if (node === null) return [];
 
-  let current = [];
+  let current:number[] = [];
   for (let child of node.children){
-    current = child.concat(preOrder(child));
+    current = current.concat(postOrder(child))
   }
 
-  return current.concat([node.val]);
+  current.push(node.val)
+  return current;
 }
 
 export { preOrder, postOrder };
