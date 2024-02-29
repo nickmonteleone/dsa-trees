e7dd4aca461efc49bf876283e312dc8235c8f22e
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-02-29 11:56:06 -0800

    Started bfs for bsts

--- bsts/bfs/bfs.ts
+++ bsts/bfs/bfs.ts
@@ -1,10 +1,23 @@
 import { BNodeNum } from "../common/bst";
+import { QueueNum } from "../common/queue";
 
 
 /** bfs(): Traverse the BST using BFS.
  * Returns an array of visited nodes. */
 
 function bfs(node: BNodeNum | null): number[] {
+
+  // if (node === null) return null;
+
+  const queue = new QueueNum([node.val])
+
+  while (node !== null) {
+    // queue.enqueue(node.left.val);
+
+
+  }
+
+
   return [42];
 }
 
