60b584e11e43ae0e0c1e4f0d0cc4dcfe1cab1ee9
Max Shpungin <maxsconsulting@gmail.com>
2024-03-01 12:15:06 -0800

    people sort in progress

--- dsa-sorting/sorting/case-insensitive/caseInsensitive.ts
+++ dsa-sorting/sorting/case-insensitive/caseInsensitive.ts
@@ -1,7 +1,12 @@
 /** Sort array of strings in-place, case-insensitively. Returns array. */
 
 function caseInsensitiveSort(arr: string[]): string[] {
-  return [];
+  arr.sort((a,b) => {
+    if (b.toUpperCase()  > a.toUpperCase()) return -1;
+    if (b.toUpperCase()  < a.toUpperCase()) return 1;
+    return 0;
+  })
+  return arr;
 }
 
 export { caseInsensitiveSort };
--- dsa-sorting/sorting/people-sort/peopleSort.ts
+++ dsa-sorting/sorting/people-sort/peopleSort.ts
@@ -1,24 +1,25 @@
-type tPerson = {fname: string, lname: string};
+p2type tPerson = { fname: string, lname: string; };
 
 /** Sort array of persons in place (lname > fname). Returns array. */
 
 function peopleSort(arr: tPerson[]): tPerson[] {
-  return arr.sort((a: tPerson, b: tPerson): number => {
-        if (a.lname < b.lname) {
-          return -1;
-        } else if (a.lname > b.lname) {
-          return +1;
-        } else {
-          if (a.fname < b.fname) {
-            return -1;
-          } else if (a.fname > b.fname) {
-            return +1;
-          } else {
-            return 0;
-          }
-        }
-      }
+
+  arr.sort((p1, p2) => {
+    if (p1.lname > p2.lname) return -1;
+    if (p1.lname < p2.lname) return 1;
+    return _sortFirstName(p1,p2)
+  }
   );
+
+  function _sortFirstName(p1,p2): number {
+
+      if (p1.fname > p2.fname) return -1;
+      if (p1.fname < p2.fname) return 1;
+      return 0;
+
+  }
+
+  return arr;
 }
 
 export { peopleSort };
