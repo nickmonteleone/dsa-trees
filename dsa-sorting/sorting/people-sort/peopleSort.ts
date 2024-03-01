31116eea9f1b40aa48ea98f950280ac58ce5cfb1
Nick Monteleone <nicholasmonteleone25@gmail.com>
2024-03-01 12:31:10 -0800

    Passes test for peopleSort

--- dsa-sorting/sorting/people-sort/peopleSort.ts
+++ dsa-sorting/sorting/people-sort/peopleSort.ts
@@ -1,23 +1,16 @@
-p2type tPerson = { fname: string, lname: string; };
+type tPerson = { fname: string, lname: string; };
 
 /** Sort array of persons in place (lname > fname). Returns array. */
 
 function peopleSort(arr: tPerson[]): tPerson[] {
 
-  arr.sort((p1, p2) => {
-    if (p1.lname > p2.lname) return -1;
-    if (p1.lname < p2.lname) return 1;
-    return _sortFirstName(p1,p2)
-  }
-  );
-
-  function _sortFirstName(p1,p2): number {
-
-      if (p1.fname > p2.fname) return -1;
-      if (p1.fname < p2.fname) return 1;
-      return 0;
-
-  }
+  arr.sort((a: tPerson, b: tPerson): number => {
+    if (a.lname < b.lname) return -1;
+    if (a.lname > b.lname) return 1;
+    if (a.fname < b.fname) return -1;
+    if (a.fname > b.fname) return 1;
+    return 0;
+  });
 
   return arr;
 }
