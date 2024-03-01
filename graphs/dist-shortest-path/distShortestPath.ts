10bb27d36b74edc1edccb64f35ce51a3f6b88376
Max Shpungin <maxsconsulting@gmail.com>
2024-02-29 16:33:30 -0800

    working on distShortestPath

--- graphs/dist-shortest-path/distShortestPath.ts
+++ graphs/dist-shortest-path/distShortestPath.ts
@@ -1,9 +1,40 @@
+import { g } from "vitest/dist/suite-UrZdHRff";
 import { GNodeStr } from "../common/graph";
+import { Queue } from "../common/queue";
+import { Stack } from "../common/stack";
 
 /** Distance of shortest path between start and end.
  * If no path exists, return Infinity. */
 
 function distShortestPath(start: GNodeStr, sought: GNodeStr): number {
+  let steps = 0;
+
+  if (start === sought) return steps;
+
+  const toVisit = new Queue([start]);
+  const visited = new Set([start]);
+
+  steps++;
+  while (!toVisit.isEmpty()) {
+    let current = toVisit.dequeue();
+    const currentToVisit = new Stack([current.adjacent]);
+
+    for (const child of current.adjacent) {
+      currentToVisit.pop();
+
+      if (!visited.has(child)) {
+        toVisit.enqueue(child);
+        visited.add(child);
+      }
+
+      if (currentToVisit.isEmpty()) {
+        steps++;
+      }
+
+    }
+
+  }
+
   return Infinity;
 }
 
